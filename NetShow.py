6eb2101b4dedb640de3961465cc2b4ee035a39af
Stallion77RepoOfficial <149940678+Stallion77RepoOfficial@users.noreply.github.com>
2024-02-16 17:53:08 +0300

    Fix Errors

--- NetShow.py
+++ NetShow.py
@@ -2,18 +2,35 @@ import socket
 import uuid
 import speedtest
 
-# IP adresini al
+def get_local_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip_address = s.getsockname()[0]
+        s.close()
+    except Exception as e:
+        print(f"Bir hata oluştu: {e}")
+        ip_address = '127.0.0.1'
+    return ip_address
+
+def get_mac_address():
+    mac_num = hex(uuid.getnode()).replace('0x', '').upper()
+    mac_address = ':'.join(mac_num[i:i + 2] for i in range(0, 11, 2))
+    return mac_address
+
+def get_speedtest_results():
+    st = speedtest.Speedtest()
+    st.get_best_server()
+    st.download()
+    st.upload()
+    st.results.share()
+    results_dict = st.results.dict()
+    return results_dict['download'] / 1e6, results_dict['upload'] / 1e6
+
 hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-
-# MAC adresini al
-mac_address = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) 
-                        for ele in range(0,8*6,8)][::-1])
-
-# Speedtest
-st = speedtest.Speedtest()
-download_speed = st.download()/1e6  # Mbps
-upload_speed = st.upload()/1e6  # Mbps
+ip_address = get_local_ip()
+mac_address = get_mac_address()
+download_speed, upload_speed = get_speedtest_results()
 
 print(f"Host Adı: {hostname}")
 print(f"IP Adresi: {ip_address}")
